1fe9b04de6f52ad82ea02293ee57f31da1e55343
Darshan Patel <91478282+darshan260802@users.noreply.github.com>
2025-02-01 05:33:12 +0000

    kk

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const cors = require("cors");
-const { publicIpv4 } = require("public-ip");
+const { publicIpv4, publicIp, publicIpv6 } = require("public-ip");
 
 const PORT = process.env.PORT || 8000;
 
@@ -13,10 +13,15 @@ app.use(express.json());
 
 app.get("/", async (req, res) => {
   const ip = await publicIpv4();
+  const i6 = await publicIpv6();
+  const ia = await publicIp();
   var ipPRIV = req.headers['x-forwarded-for'] || req.socket.remoteAddress
 
   res.json({
     loc: ipPRIV,
+    add: req.socket.remoteAddress. 
+    ia,
+    i6,
     ip: ip
   })
 });
