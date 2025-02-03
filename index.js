5b52d1224ba8e1cf113d5b5d46c3c479afba972a
Darshan Patel <91478282+darshan260802@users.noreply.github.com>
2025-02-03 17:54:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,27 +11,13 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  const publicIP = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const publicIP = (req.headers['x-forwarded-for'] ?? "").split(',')[0];
   
-  // Ensure the IP is public
-  const isPublicIP = (ip) => {
-    const privateRanges = [
-      /^10\./,
-      /^172\.(1[6-9]|2[0-9]|3[0-1])\./,
-      /^192\.168\./,
-      /^127\./,
-      /^::1$/,
-      /^fc00:/,
-      /^fe80:/
-    ];
-    return !privateRanges.some((range) => range.test(ip));
-  };
-
-  if (isPublicIP(publicIP)) {
-    res.json({ publicIP });
-  } else {
-    res.status(200).json({ error: "Could not determine public IP address", publicIP });
+  if(!publicIP){
+    res.status(400).json({error: {message: "IP NOT FOUND !!!"}});
+    return;
   }
+  res.status(200).json({IP: publicIP})
 });
 
 app.listen(PORT, () => {
